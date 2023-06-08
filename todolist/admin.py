7601926304e4aee2282b74bdfabe8a68872caac4
S0l3k <turtleteamwcg@gmail.com>
2023-06-08 15:54:30 +0300

    создал: список дел для адмим-панели добавил: вход, выход и создание пользователя

--- todolist/admin.py
+++ todolist/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import ToDo
 
-# Register your models here.
+class ToDoAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_created', )
+
+admin.site.register(ToDo, ToDoAdmin)
--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
 
+
+def home(request):
+    return render(request, 'todolist/home.html')
 def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todolist/signupuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todolist/signupuser.html',
+                              {'form': UserCreationForm(),
+                               'error': 'That username has already been taken. Please, choose another username'})
+        else:
+            return render(request, 'todolist/signupuser.html',
+                          {'form': UserCreationForm(),
+                           'error': 'Password did not match'})
 
-    return render(request, 'todolist/signupuser.html', {'form': UserCreationForm()})
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todolist/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todolist/loginuser.html',
+                          {'form': AuthenticationForm(),
+                           'error':'Username and passeword did not match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+def currenttodos(request):
+    return render(request, 'todolist/currenttodos.html')
